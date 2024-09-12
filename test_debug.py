f8a604f77a9e1c46944096f4b74df517a11f2ab2
karthikeya7746 <104351752+karthikeya7746@users.noreply.github.com>
2024-09-12 15:31:32 -0400

    Add files via upload

---
+++ test_debug.py
@@ -0,0 +1,16 @@
+from hw2_debugging import merge_sort
+
+def test_p():
+    a = [9,4,5,2,1,10,3]
+    expected = [1,2,3,4,5,9,10]
+    assert merge_sort(a) == expected
+
+def test_n():
+    a = [-22,-9,-15,-8,-2,0]
+    expected = [-22,-15,-9,-8,-2,0]
+    assert merge_sort(a) == expected
+
+def test_d():
+    a = [2,3,5,5,5,3,3,2]
+    expected = [2,2,3,3,3,5,5,5]
+    assert merge_sort(a) == expected
