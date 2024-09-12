4664ab5937304c5946cae7aaa962e9a3fd2946fa
karthikeya7746 <104351752+karthikeya7746@users.noreply.github.com>
2024-09-12 15:35:04 -0400

    Update test_debug.py

--- test_debug.py
+++ test_debug.py
@@ -1,16 +1,20 @@
+'''the'''
 from hw2_debugging import merge_sort
 
 def test_p():
+    '''the'''
     a = [9,4,5,2,1,10,3]
     expected = [1,2,3,4,5,9,10]
     assert merge_sort(a) == expected
 
 def test_n():
+    '''the'''
     a = [-22,-9,-15,-8,-2,0]
     expected = [-22,-15,-9,-8,-2,0]
     assert merge_sort(a) == expected
 
 def test_d():
+    '''the'''
     a = [2,3,5,5,5,3,3,2]
     expected = [2,2,3,3,3,5,5,5]
     assert merge_sort(a) == expected
