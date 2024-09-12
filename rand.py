6abaed1f674bbfa916738a93f90cab9222e54735
bittubharadwaj <129821633+bittubharadwaj@users.noreply.github.com>
2024-09-12 15:26:15 -0400

    Update rand.py

--- rand.py
+++ rand.py
@@ -1,10 +1,11 @@
 ''' 
 The function below generate an array of random numbers using shuf 
 '''
-import sys,logging
+import logging
 import subprocess
 
-logging.basicConfig(filename='rand_debugging.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s  %(message)s')
+logging.basicConfig(filename='rand_debugging.log', encoding='utf-8', level=logging.DEBUG,
+                    format='%(asctime)s  %(message)s')
 logging.debug("NEW LOG RAND File" )
 def random_array(arr):
     '''
@@ -12,7 +13,6 @@ def random_array(arr):
     '''
     logger = logging.getLogger(__name__)
     shuffled_num = None
-  
     logger.debug("shuffled_num: %r",shuffled_num)
     # for i in range(len(arr)):
 
@@ -21,7 +21,6 @@ def random_array(arr):
 
         shuffled_num = subprocess.run(
             ["/opt/homebrew/bin/shuf", "-i1-20", "-n1"], capture_output=True,check=True)
-        
         # logger.debug("shuffled stdout: %r",shuffled_num)
         arr[i] = int(shuffled_num.stdout)
         logger.debug("Assigned random number %r to arr[%r]", arr[i],i)
