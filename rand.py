0725c6af25dc6babcce2d04889ca0149799e5978
dubeavik002 <159172128+dubeavik002@users.noreply.github.com>
2024-09-12 14:27:57 -0400

    debugging for rand,py and hw2_debugging.py

--- rand.py
+++ rand.py
@@ -1,25 +1,29 @@
-"""
-This module contains functions for generating random numbers
-and filling arrays with random values using system commands.
-"""
-
+''' 
+The function below generate an array of random numbers using shuf 
+'''
+import sys,logging
 import subprocess
 
-
+logging.basicConfig(filename='rand_debugging.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s  %(message)s')
+logging.debug("NEW LOG RAND File" )
 def random_array(arr):
-    """
-    Populates the given array with random numbers using the 'shuf' command.
-
-    Args:
-        arr (list): A list to be filled with random integers.
-
-    Returns:
-        list: The input array filled with random integers.
-    """
+    '''
+    Shuffles an array by replacing its element with random integers using shuf command 
+    '''
+    logger = logging.getLogger(__name__)
     shuffled_num = None
+  
+    logger.debug("shuffled_num: %r",shuffled_num)
+    # for i in range(len(arr)):
+
     for i, _ in enumerate(arr):
+        logger.debug("Processing index: %r",i)
+
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True
-        )
+            ["/opt/homebrew/bin/shuf", "-i1-20", "-n1"], capture_output=True,check=True)
+        
+        # logger.debug("shuffled stdout: %r",shuffled_num)
         arr[i] = int(shuffled_num.stdout)
+        logger.debug("Assigned random number %r to arr[%r]", arr[i],i)
+    logger.debug("final arr: %r",arr)
     return arr
