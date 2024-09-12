984e98f5eedce6f10f4373f192e8c8c77ab9fe2a
karthikeya7746 <104351752+karthikeya7746@users.noreply.github.com>
2024-09-12 15:40:25 -0400

    Update test_debug.py

--- test_debug.py
+++ test_debug.py
@@ -1,20 +1,20 @@
-'''the'''
+'''Test cases for mergesort'''
 from hw2_debugging import merge_sort
 
 def test_p():
-    '''the'''
+    '''Consider positive numbers'''
     a = [9,4,5,2,1,10,3]
     expected = [1,2,3,4,5,9,10]
     assert merge_sort(a) == expected
 
 def test_n():
-    '''the'''
+    '''Consider negative numbers'''
     a = [-22,-9,-15,-8,-2,0]
     expected = [-22,-15,-9,-8,-2,0]
     assert merge_sort(a) == expected
 
 def test_d():
-    '''the'''
+    '''Consider duplicates'''
     a = [2,3,5,5,5,3,3,2]
     expected = [2,2,3,3,3,5,5,5]
     assert merge_sort(a) == expected
